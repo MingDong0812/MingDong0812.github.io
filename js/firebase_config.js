9949b6376afc6a0eca4b454175d10eabe47572ae
MingDong0812 <129807069+MingDong0812@users.noreply.github.com>
2023-08-24 12:50:55 +0900

    Update firebase_config.js

--- js/firebase_config.js
+++ js/firebase_config.js
@@ -8,14 +8,6 @@ import { getDatabase, ref, onChildAdded, push, set, child } from 'https://www.gs
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBFuLM6deyXq5LkQMrfSNNSs_2X9qlP6Sc",
-    authDomain: "mingdong0812-3d5ff.firebaseapp.com",
-    projectId: "mingdong0812-3d5ff",
-    storageBucket: "mingdong0812-3d5ff.appspot.com",
-    messagingSenderId: "460986178104",
-    appId: "1:460986178104:web:96787838eb9b379262e4cd",
-    measurementId: "G-535TFJRH2V",
-    databaseURL: "https://mingdong0812-3d5ff-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 
 const app = initializeApp(firebaseConfig);
